32392941010418df5332647d335f26462efb2375
JWN <naa3000@naver.com>
2025-03-27 23:35:30 +0900

    first commit

---
+++ src/main/java/jwn/item_checklist/ItemChecklistClient.java
@@ -0,0 +1,12 @@
+package jwn.item_checklist;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItemChecklistClient implements ClientModInitializer {
+	public static final String MOD_ID = "item_checklist";
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
